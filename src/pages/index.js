1f86c93eac8049b706533ec45c4ea6eb3f2b6e81
Subh <subhakant@staragrifinance.com>
2021-06-23 13:36:15 +0530

    calling the Herocomponent

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,32 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
-import { StaticImage } from 'gatsby-plugin-image'
-
+import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 import Seo from '../components/seo'
+import HeroSection from '../components/Reusable/HeroSection'
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <Seo title='Home' />
+    <HeroSection
+      // as 'data' is coming as default from the graphql query so we are passing data as a prop
+      img={data.fileName.childImageSharp.fluid}
+      title='I write Code'
+      subtitle='Learn Code Online'
+      heroclass='hero-background'
+    />
   </Layout>
 )
+
+export const query = graphql`
+  {
+    fileName: file(relativePath: { eq: "heromain.png" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
